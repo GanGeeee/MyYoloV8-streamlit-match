492455bc4abbd74599e2f5fe2ab6dae9ee9d30f3
GanGeeee <150319583+GanGeeee@users.noreply.github.com>
2026-06-28 00:53:49 +0800

    Delete ultralytics directory

--- ultralytics/__init__.py
+++
@@ -1,9 +0,0 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
-__version__ = "8.0.5"
-
-from ultralytics.hub import checks
-from ultralytics.yolo.engine.model import YOLO
-from ultralytics.yolo.utils import ops
-
-__all__ = ["__version__", "YOLO", "hub", "checks"]  # allow simpler import
